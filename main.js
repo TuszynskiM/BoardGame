c02317d37cca6ca31b59b80993ef9d657cdcbcb5
michal_tuszynski <tuszynski.michal@o2.pl>
2019-09-05 15:44:49 +0200

    Update board

--- main.js
+++ main.js
@@ -1,65 +1,70 @@
 const throwBtn = document.querySelector('.throw');
-const boardWraper = document.querySelector('.board');
+const boardWraper = document.querySelectorAll('.field');
+const pawn = document.querySelector('.pawn');
+const dice = document.getElementById('diceID');
+
 
 // Special field:
 // string - title to show
 // number - move the pawn by this value 
 const board = [
-    {isActive: true, specialField:0},
-    {isActive:false},
-    {isActive:false},
-    {isActive:false},
-    {isActive:false},
-    {isActive:false},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false,specialField:'Przegraną'},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false,},
-    {isActive:false, specialField:-8},
-    {isActive:false, specialField:'Wygraną'},
+    { isActive: true, specialField: 0 },
+    { isActive: false },
+    { isActive: false },
+    { isActive: false },
+    { isActive: false },
+    { isActive: false },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, specialField: 'Przegraną' },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, },
+    { isActive: false, specialField: -8 },
+    { isActive: false, specialField: 'Wygraną' },
 ]
 
-const pawn = {currentField:0, numberOfThrows:0, numberOfStitches: 0}
+const pawnInfo = { currentField: 0, numberOfThrows: 0, numberOfStitches: 0 }
 
 const throwCube = () => {
     let throwedNumber = Math.floor((Math.random() * 5) + 1);
-    pawn.numberOfThrows +=1;
-    pawn.numberOfStitches += throwedNumber;
+    pawnInfo.numberOfThrows += 1;
+    pawnInfo.numberOfStitches += throwedNumber;
+    dice.style.backgroundPositionX = `${-23 - ((throwedNumber - 1) * 153)}px`
     movePawn(throwedNumber);
 }
 
 const movePawn = (number) => {
-    board[pawn.currentField].isActive= false;
-    boardWraper.children[pawn.currentField].classList.remove('active');
-    pawn.currentField += number;
-    if(pawn.currentField > 20){
-        pawn.currentField = 20 - (pawn.currentField - 20);
-    }    
-    boardWraper.children[pawn.currentField].classList.add('active');
-    board[pawn.currentField].isActive= true;
+    board[pawnInfo.currentField].isActive = false;
+    pawnInfo.currentField += number;
+    if (pawnInfo.currentField > 20) {
+        pawnInfo.currentField = 20 - (pawnInfo.currentField - 20);
+    }
+    boardWraper[pawnInfo.currentField].appendChild(pawn);
+    board[pawnInfo.currentField].isActive = true;
     result();
 }
 
 const result = () => {
     let activeField = board.filter(field => field.isActive);
     let field = activeField[0].specialField;
-    if( typeof field === 'string') {
-        alert(`Zakończyłeś gre: ${field}, liczba rzutów: ${pawn.numberOfThrows}, średnia liczba oczek na rzut: ${Math.round(pawn.numberOfStitches / pawn.numberOfThrows)}`);
+    if (typeof field === 'string') {
+        alert(`Zakończyłeś gre: ${field}, liczba rzutów: ${pawnInfo.numberOfThrows}, średnia liczba oczek na rzut: ${Math.round(pawnInfo.numberOfStitches / pawnInfo.numberOfThrows)}`);
         location.reload();
-    } else if(typeof field === 'number'){
+    } else if (typeof field === 'number') {
         movePawn(field);
     }
 }
 
+
 throwBtn.addEventListener('click', throwCube)
+
 
 
