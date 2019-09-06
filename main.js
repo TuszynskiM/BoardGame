82ecac06d3f9904089aa934dfc2d7fb086cda0b4
michal_tuszynski <tuszynski.michal@o2.pl>
2019-09-06 11:50:07 +0200

    Added shadow

--- main.js
+++ main.js
@@ -38,17 +38,21 @@ const throwCube = () => {
     pawnInfo.numberOfThrows += 1;
     pawnInfo.numberOfStitches += throwedNumber;
     dice.style.backgroundPositionX = `${-23 - ((throwedNumber - 1) * 153)}px`
-    movePawn(throwedNumber);
+    pawnPosition(throwedNumber);
+
 }
 
-const movePawn = (number) => {
+const pawnPosition = (number) => {
     board[pawnInfo.currentField].isActive = false;
+
+    movePawn(pawnInfo.currentField, pawnInfo.currentField + number);
+
     pawnInfo.currentField += number;
     if (pawnInfo.currentField > 20) {
         pawnInfo.currentField = 20 - (pawnInfo.currentField - 20);
     }
-    boardWraper[pawnInfo.currentField].appendChild(pawn);
     board[pawnInfo.currentField].isActive = true;
+
     result();
 }
 
@@ -56,13 +60,21 @@ const result = () => {
     let activeField = board.filter(field => field.isActive);
     let field = activeField[0].specialField;
     if (typeof field === 'string') {
-        alert(`Zakończyłeś gre: ${field}, liczba rzutów: ${pawnInfo.numberOfThrows}, średnia liczba oczek na rzut: ${Math.round(pawnInfo.numberOfStitches / pawnInfo.numberOfThrows)}`);
-        location.reload();
+        console.log(`Zakończyłeś gre: ${field}, liczba rzutów: ${pawnInfo.numberOfThrows}, średnia liczba oczek na rzut: ${Math.round(pawnInfo.numberOfStitches / pawnInfo.numberOfThrows)}`);
     } else if (typeof field === 'number') {
-        movePawn(field);
+        pawnPosition(field);
     }
 }
 
+
+const movePawn = (i, max) => {
+    while (i < max) {
+        i++;
+        setTimeout(() => {
+            boardWraper[i].appendChild(pawn);
+        }, 1000)
+    }
+}
 
 throwBtn.addEventListener('click', throwCube)
 
