3bc00b45efc3fb74b54a369c7956ce55d2bbcd9a
michal_tuszynski <tuszynski.michal@o2.pl>
2019-09-10 17:18:29 +0200

    pawn movement

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const throwBtn = document.querySelector(".throwBtn");
 const closeBtn = document.querySelector(".closeBtn");
 const boardWraper = document.querySelectorAll(".field");
-const pawn = document.querySelector(".pawn");
+const pawn = document.getElementById("pawnID");
 const dice = document.getElementById("diceID");
 const results = document.querySelector(".results");
 
@@ -9,30 +9,44 @@ const results = document.querySelector(".results");
 // string - title to show
 // number - move the pawn by this value
 const board = [
-  { isActive: true, specialField: 0 },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false, specialField: "Przegrana" },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false },
-  { isActive: false, specialField: -8 },
-  { isActive: false, specialField: "Wygrana" }
+  { isActive: true ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false, specialField: "Przegrana" ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false ,top:"", left:""},
+  { isActive: false, specialField: -8 ,top:"", left:""},
+  { isActive: false, specialField: "Wygrana" ,top:"", left:""}
 ];
 
 const pawnInfo = { currentField: 0, numberOfThrows: 0, numberOfStitches: 0 };
+
+const getCoordinates = () => {
+    boardWraper.forEach((fieldDiv, index) => {
+      let coordinates = fieldDiv.getBoundingClientRect();
+      board[index].top = `${coordinates.top}px`;  
+      board[index].left = `${coordinates.left}px`;
+    });
+};
+
+(function init(){
+  getCoordinates();
+  pawn.style.top = board[0].top;
+  pawn.style.left = board[0].left;
+})();
 
 const throwCube = () => {
   let throwedNumber = Math.floor(Math.random() * 6 + 1);
@@ -43,16 +57,20 @@ const throwCube = () => {
 };
 
 const pawnPosition = number => {
+
   board[pawnInfo.currentField].isActive = false;
-
-  //movePawn(pawnInfo.currentField, pawnInfo.currentField + number);
-
   pawnInfo.currentField += number;
+
+  //let endPoint = pawnInfo.currentField + number;
+  //movePawn(endPoint);
+
   if (pawnInfo.currentField > 20) {
     pawnInfo.currentField = 20 - (pawnInfo.currentField - 20);
   }
+
+  movePawn(pawnInfo.currentField-number,pawnInfo.currentField);
+
   board[pawnInfo.currentField].isActive = true;
-  boardWraper[pawnInfo.currentField].appendChild(pawn);
   result();
 };
 
@@ -60,23 +78,32 @@ const result = () => {
   let activeField = board.filter(field => field.isActive);
   let field = activeField[0].specialField;
   if (typeof field === "string") {
-    results.children[0].textContent = `${field}`
+    results.children[0].textContent = field;
     results.children[1].textContent = `Liczba rzutów: ${pawnInfo.numberOfThrows}`
-    results.children[2].textContent = ` średnia liczba oczek: ${Math.round(pawnInfo.numberOfStitches / pawnInfo.numberOfThrows)}`
+    results.children[2].textContent = ` Srednia liczba oczek: ${Math.round(pawnInfo.numberOfStitches / pawnInfo.numberOfThrows)}`
     results.classList.toggle("hideResults");
   } else if (typeof field === "number") {
     pawnPosition(field);
   }
 };
 
-const movePawn = (i, max) => {
-  while (i < max) {
-    i++;
-    setTimeout(() => {
-      boardWraper[i].appendChild(pawn);
-    }, 1000);
+
+const movePawn = (startPoint, endPoint)=> {
+  for(let i = startPoint; i <= endPoint; i++){
+  pawn.style.top = board[i].top;
+  pawn.style.left = board[i].left;
+  sleep(500);
+}
+}
+
+function sleep(milliseconds) {
+  const start = new Date().getTime();
+  for (let i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
   }
-};
+}
 
 throwBtn.addEventListener("click", throwCube);
 
